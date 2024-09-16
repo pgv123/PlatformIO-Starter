1e2778716ddba89ed06b5bc82d0f7708b01dcffa
pgv123 <32214613+pgv123@users.noreply.github.com>
2024-09-16 20:38:33 +1000

    Started Dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,52 @@
 #include <FastLED.h>
 
-#define LED_PIN     5
-#define NUM_LEDS    1
-#define BRIGHTNESS  255
+#define LED_PIN1     4
+#define LED_PIN2     5
+#define NUM_LEDS    10
+#define TOT_LEDS    20
+#define STEPS       2
+#define BRIGHTNESS  100
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
-CRGB leds[NUM_LEDS];
+CRGB led1[TOT_LEDS];
+CRGB led2[TOT_LEDS];
 
 void setup() {
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, LED_PIN1, COLOR_ORDER>(led1, TOT_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, LED_PIN2, COLOR_ORDER>(led2, TOT_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
+  Serial.begin(9600);
 }
 
 void loop() {
-  leds[0] = CRGB::Red;
+  int i, step_count;
+  long j, j1;
+  Serial.println("*");
+  
+  for(i=0;i < TOT_LEDS;i++) {
+    led1[i] = CRGB::Black;
+    led2[i] = CRGB::Black;    
+
+  
+  }
+    FastLED.show();
+    delay(150);
+j = 0;
+j1 = 0;
+for(step_count = 0; step_count < STEPS; step_count++){
+ //randomSeed(analogRead(0));
+ j = j + random(NUM_LEDS);
+   j1 = j1 + random(NUM_LEDS);
+
+ for (i=0;i < TOT_LEDS;i++){
+  if (i < j) led1[i] = CRGB::Blue;
+  else led1[i] = CRGB:: Black;
+  if (i < j1) led2[i] = CRGB::Purple;
+  else led2[i] = CRGB:: Black;
   FastLED.show();
+  delay(100);
+ } 
+
   delay(1000);
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(1000);
+}
 }
