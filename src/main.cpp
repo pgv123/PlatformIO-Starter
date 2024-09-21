54b85044d540d17a09e918c83ba9c4cf5a68c051
pgv123 <32214613+pgv123@users.noreply.github.com>
2024-09-21 20:09:05 +1000

    Added Cylon

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <FastLED.h>
 
+#define SHOW_LOOPS  50
 #define LED_PIN1     4
 #define LED_PIN2     5
 #define LED_PIN3     16
@@ -7,7 +8,7 @@
 #define LED_PIN5     12
 #define LED_PIN6     14
 #define NUM_LEDS    10
-#define TOT_LEDS    92
+#define TOT_LEDS    30
 #define STEPS       10
 #define BRIGHTNESS  100
 #define LED_TYPE    WS2812
@@ -27,17 +28,54 @@ void setup() {
   FastLED.addLeds<LED_TYPE, LED_PIN5, COLOR_ORDER>(led5, TOT_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.addLeds<LED_TYPE, LED_PIN6, COLOR_ORDER>(led6, TOT_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
-  Serial.begin(9600);
+  Serial.begin(115200);
+}
+
+void fadeall() { for(int i = 0; i < TOT_LEDS; i++) { led1[i].nscale8(250); } }
+
+void cylon(int loops){
+    static uint8_t hue = 0;
+    for(int j=0; j<loops; j++){
+    Serial.print(" ");
+    Serial.print(j);
+    // First slide the led in one direction
+    for(int i = 0; i < TOT_LEDS; i++) {
+        // Set the i'th led to red 
+        led1[i] = CHSV(hue++, 255, 255);
+        // Show the leds
+        FastLED.show(); 
+        // now that we've shown the leds, reset the i'th led to black
+        // leds[i] = CRGB::Black;
+        fadeall();
+        // Wait a little bit before we loop around and do it again
+        delay(10);
+    }
+    Serial.print("x");
+ 
+    // Now go in the other direction.  
+    for(int i = (TOT_LEDS)-1; i >= 0; i--) {
+        // Set the i'th led to red 
+        led1[i] = CHSV(hue++, 255, 255);
+        // Show the leds
+        FastLED.show();
+        // now that we've shown the leds, reset the i'th led to black
+        // leds[i] = CRGB::Black;
+        fadeall();
+        // Wait a little bit before we loop around and do it again
+        delay(10);
+    }
+    }
 }
 
 void loop() {
   int i, step_count;
-  long horse_pos[6];
+  long horse_pos[6] = {0,0,0,0,0,0};
   Serial.println("*");
   
   FastLED.clear();
   FastLED.show();
   delay(150);
+  cylon(SHOW_LOOPS);
 
 for(step_count = 0; step_count < STEPS; step_count++){
     //increment each horsey randomly
